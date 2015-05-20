14903ee104cd60c2c72cf95fa5e1435764052e95
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-05-20 17:08:07 -0600

    user edits existing task feature test

---
+++ test/features/user_edits_task_test.rb
@@ -0,0 +1,18 @@
+require_relative "../test_helper"
+
+class EditTaskTest < FeatureTest
+  def test_user_can_edit_existing_task
+    TaskManager.create(title: "Practice Capybara", description: "tonight")
+    visit "/tasks"
+    click_link_or_button("Edit") # Ask!!
+    assert "/tasks/1/edit", current_path # Ask Why test passes with or without 1
+
+    fill_in("task[title]", :with => "Review")
+    fill_in("task[description]", :with => "Go over classwork")
+    click_link_or_button("submit")
+    assert "/tasks", current_path
+
+    assert page.has_content?("Review")
+    refute page.has_content?("Blah")
+  end
+end
